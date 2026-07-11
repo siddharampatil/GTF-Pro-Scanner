677474a0a48cc0bcddb7a6fc60e575194d718c8f
siddharampatil <120033317+siddharampatil@users.noreply.github.com>
2026-07-12 01:07:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-  
 import requests
 
 BOT_TOKEN = "8600535028:AAG1DqnJt66Uj034SBhpEfWgUjHFBSrOiwA"
 CHAT_ID = "902764304"
 
-def send_message(message):
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    requests.post(
-        url,
-        data={
-            "chat_id": CHAT_ID,
-            "text": message
-        }
-    )
+print("Starting Telegram test...")
 
-if __name__ == "__main__":
-    send_message("✅ GTF Pro Swing Scanner is running from GitHub Actions!")
-    print("Message sent successfully.")
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
+response = requests.post(
+    url,
+    data={
+        "chat_id": CHAT_ID,
+        "text": "✅ GitHub Actions test successful!"
+    }
+)
+
+print("Status Code:", response.status_code)
+print("Response:", response.text)
