d028e7afb2bc389adb3d39ea2cc7668bce916fa1
siddharampatil <120033317+siddharampatil@users.noreply.github.com>
2026-07-13 01:00:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,25 +8,19 @@ from ta.momentum import RSIIndicator
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-message = """
-📊 GTF PRO SCANNER
-
-Scanner Status: ✅ Running
-
-Market: NSE
-Strategy: EMA + RSI + Volume
-
-No trading setup found at this scan.
-
-Next scan in 15 minutes.
-"""
-
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-requests.post(
-    url,
-    data={
-        "chat_id": CHAT_ID,
-        "text": message
-    }
-)
+stocks = [
+    "RELIANCE.NS",
+    "TCS.NS",
+    "INFY.NS",
+    "HDFCBANK.NS",
+    "ICICIBANK.NS",
+    "SBIN.NS",
+    "LT.NS",
+    "BHARTIARTL.NS",
+    "ITC.NS",
+    "AXISBANK.NS"
+]
+
+results = []
