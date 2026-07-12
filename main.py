6b4ec2314a30c70c713b0588e933d38bb1af4784
siddharampatil <120033317+siddharampatil@users.noreply.github.com>
2026-07-13 00:43:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
+import os
 import requests
 
-import os
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-print("Starting Telegram test...")
+message = """
+📊 GTF PRO SCANNER
+
+Scanner Status: ✅ Running
+
+Market: NSE
+Strategy: EMA + RSI + Volume
+
+No trading setup found at this scan.
+
+Next scan in 15 minutes.
+"""
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-response = requests.post(
+requests.post(
     url,
     data={
         "chat_id": CHAT_ID,
-        "text": "✅ GitHub Actions test successful!"
+        "text": message
     }
 )
-
-print("Status Code:", response.status_code)
-print("Response:", response.text)
