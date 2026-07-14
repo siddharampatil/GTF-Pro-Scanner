b481406cfcd7c460802fc9da8aff9d15286382b8
siddharampatil <120033317+siddharampatil@users.noreply.github.com>
2026-07-14 18:08:31 +0530

    Update stock_list.py

--- stock_list.py
+++ stock_list.py
@@ -1,4 +1,3 @@
 def get_stock_list():
     with open("stocks.txt", "r") as f:
-        stocks = [line.strip() for line in f if line.strip()]
-    return stocks
+        return [line.strip() for line in f if line.strip()]
