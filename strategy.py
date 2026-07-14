3f86ee7f3fe7054233f3a0d685e91c7bce7d13d7
siddharampatil <120033317+siddharampatil@users.noreply.github.com>
2026-07-14 17:30:43 +0530

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,5 +1,5 @@
 import yfinance as yf
-from ta.trend import EMAIndicator
+from ta.trend import EMAIndicator, MACD
 from ta.momentum import RSIIndicator
 from ta.volatility import AverageTrueRange
 
@@ -13,7 +13,7 @@ def scan_stock(symbol):
             auto_adjust=True
         )
 
-        if len(df) < 60:
+        if len(df) < 200:
             return None
 
         close = df["Close"].squeeze()
@@ -23,26 +23,52 @@ def scan_stock(symbol):
 
         ema20 = EMAIndicator(close, window=20).ema_indicator()
         ema50 = EMAIndicator(close, window=50).ema_indicator()
+        ema200 = EMAIndicator(close, window=200).ema_indicator()
+
         rsi = RSIIndicator(close, window=14).rsi()
-        atr = AverageTrueRange(high, low, close, window=14).average_true_range()
+
+        macd = MACD(close)
+        macd_line = macd.macd()
+        signal_line = macd.macd_signal()
+
+        atr = AverageTrueRange(
+            high,
+            low,
+            close,
+            window=14
+        ).average_true_range()
+
         avg_volume = volume.rolling(20).mean()
 
         score = 0
 
+        # Price above EMA20
         if close.iloc[-1] > ema20.iloc[-1]:
-            score += 20
+            score += 15
 
+        # EMA20 above EMA50
         if ema20.iloc[-1] > ema50.iloc[-1]:
+            score += 15
+
+        # EMA50 above EMA200
+        if ema50.iloc[-1] > ema200.iloc[-1]:
             score += 20
 
+        # RSI Strength
         if 55 <= rsi.iloc[-1] <= 70:
-            score += 20
+            score += 15
 
+        # Volume Confirmation
         if volume.iloc[-1] > avg_volume.iloc[-1]:
-            score += 20
+            score += 15
 
+        # MACD Bullish
+        if macd_line.iloc[-1] > signal_line.iloc[-1]:
+            score += 10
+
+        # 20 Day Breakout
         if close.iloc[-1] >= high.iloc[-20:].max():
-            score += 20
+            score += 10
 
         buy = round(float(close.iloc[-1]), 2)
 
@@ -52,21 +78,4 @@ def scan_stock(symbol):
 
         risk = buy - stop_loss
 
-        target1 = round(buy + risk, 2)
-        target2 = round(buy + (2 * risk), 2)
-        target3 = round(buy + (3 * risk), 2)
-
-        return {
-            "symbol": symbol.replace(".NS", ""),
-            "score": score,
-            "buy": buy,
-            "sl": stop_loss,
-            "t1": target1,
-            "t2": target2,
-            "t3": target3,
-            "rsi": round(float(rsi.iloc[-1]), 2),
-        }
-
-    except Exception as e:
-        print(f"{symbol}: {e}")
-        return None
+        target1 = round(buy + risk, 
