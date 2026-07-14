9336335e08df9a86e426f8c5a83c9b37ef072925
siddharampatil <120033317+siddharampatil@users.noreply.github.com>
2026-07-14 22:43:06 +0530

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -13,6 +13,7 @@ def scan_stock(symbol):
             auto_adjust=True
         )
 
+        # Need at least 200 days for EMA200
         if len(df) < 200:
             return None
 
@@ -21,6 +22,8 @@ def scan_stock(symbol):
 
         ema20 = EMAIndicator(close, window=20).ema_indicator()
         ema50 = EMAIndicator(close, window=50).ema_indicator()
+        ema200 = EMAIndicator(close, window=200).ema_indicator()
+
         rsi = RSIIndicator(close, window=14).rsi()
 
         avg_volume = volume.rolling(20).mean()
@@ -29,29 +32,33 @@ def scan_stock(symbol):
 
         # Price above EMA20
         if close.iloc[-1] > ema20.iloc[-1]:
-            score += 25
+            score += 20
 
         # EMA20 above EMA50
         if ema20.iloc[-1] > ema50.iloc[-1]:
-            score += 25
+            score += 20
+
+        # EMA50 above EMA200
+        if ema50.iloc[-1] > ema200.iloc[-1]:
+            score += 20
 
         # RSI Bullish
         if 50 <= rsi.iloc[-1] <= 70:
-            score += 25
+            score += 20
 
         # Volume above average
         if volume.iloc[-1] > avg_volume.iloc[-1]:
-            score += 25
+            score += 20
 
         buy = round(float(close.iloc[-1]), 2)
 
-        # Stop Loss = Lower of 5-day low or 2% below Buy
+        # Stop Loss = Lower of last 5-day low or 2% below Buy
         last5_low = round(float(close.iloc[-5:].min()), 2)
         percent_sl = round(buy * 0.98, 2)
 
         sl = min(last5_low, percent_sl)
 
-        # Ensure SL is always below Buy
+        # Ensure Stop Loss is always below Buy
         if sl >= buy:
             sl = percent_sl
 
