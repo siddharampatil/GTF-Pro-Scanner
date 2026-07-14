af2bad77f918cb3b373a64ac516e2d29bcf644b2
siddharampatil <120033317+siddharampatil@users.noreply.github.com>
2026-07-14 16:56:46 +0530

    Update stock_list.py

--- stock_list.py
+++ stock_list.py
@@ -1,12 +1,8 @@
-import pandas as pd
-
 def get_stock_list():
-    url = "https://archives.nseindia.com/content/equities/EQUITY_L.csv"
-    df = pd.read_csv(url)
-
-    symbols = []
-
-    for symbol in df["SYMBOL"]:
-        symbols.append(symbol + ".NS")
-
-    return symbols
+    return [
+        "RELIANCE.NS",
+        "TCS.NS",
+        "INFY.NS",
+        "HDFCBANK.NS",
+        "ICICIBANK.NS"
+    ]
